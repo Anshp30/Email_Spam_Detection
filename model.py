dabe579fe7647d27b235724d40eaf9db6bb8f314
pansh30 <ansh.patel30092004@gmail.com>
2026-01-30 20:26:16 +0530

    Intial commit

---
+++ model.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import pickle
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+# Load dataset
+data = pd.read_csv("dataset/spam.csv", encoding="latin-1")
+
+
+
+
+# Convert labels
+data['label'] = data['label'].map({'ham': 0, 'spam': 1})
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(
+    data['mesage'], data['label'], test_size=0.2, random_state=42
+)
+
+# Vectorization
+vectorizer = TfidfVectorizer(stop_words=stopwords.words('english'))
+X_train_vec = vectorizer.fit_transform(X_train)
+X_test_vec = vectorizer.transform(X_test)
+
+# Model
+model = MultinomialNB()
+model.fit(X_train_vec, y_train)
+
+# Accuracy
+y_pred = model.predict(X_test_vec)
+print("Model Accuracy:", accuracy_score(y_test, y_pred))
+
+# Save model
+pickle.dump(model, open("spam_model.pkl", "wb"))
+pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
