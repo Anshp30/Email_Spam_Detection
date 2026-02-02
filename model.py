b57e00e26ca830121ab8437df5bfea5e52b37689
pansh30 <ansh.patel30092004@gmail.com>
2026-02-02 20:58:43 +0530

    intial commit

--- model.py
+++ model.py
@@ -23,7 +23,7 @@ data['label'] = data['label'].map({'ham': 0, 'spam': 1})
 
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(
-    data['message'], data['label'], test_size=0.2, random_state=42
+    data['mesage'], data['label'], test_size=0.2, random_state=42
 )
 
 # Vectorization
